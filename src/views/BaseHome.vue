42cdf0f0c8b659969f6c1b618289ee1f7f778f58
chachacharlie <carlos.torres@scisa.com>
2025-07-22 10:49:49 -0600

    ADD: Añadir composable para debounce general

--- src/views/BaseHome.vue
+++ src/views/BaseHome.vue
@@ -1,7 +1,14 @@
 <template>
   <section>
     <h1>Base Home</h1>
+
   </section>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import DashButton from '@/components/buttons/DashButton.vue';
+import { useDebounce } from '@/composables/useDebounce';
+
+const { debounce } = useDebounce({timeDebounce: 1000});
+
+</script>
